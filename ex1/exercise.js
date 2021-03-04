ff798cad5e03c01f4dd114a480f1253a5f3992dd
Siar Sohrab <siar.sohrab@gmail.com>
2021-03-04 12:55:24 +0100

    done ex1

--- ex1/exercise.js
+++ ex1/exercise.js
@@ -1,3 +1,23 @@
 // define your functions here
 
 // call the functions and connect them to an event
+
+var strong = document.querySelectorAll('strong');
+var a = document.querySelector('a');
+console.log(strong);
+a.addEventListener('mouseenter', function () {
+    strong.forEach(function (str) {
+            str.style.fontSize = '60px';
+        });
+    
+});
+
+var strong = document.querySelectorAll('strong');
+var a = document.querySelector('a');
+console.log(strong);
+a.addEventListener('mouseleave', function () {
+    strong.forEach(function (str) {
+            str.style.fontSize = '30px';
+        });
+    
+});
